cdf8d14844596d9a5836d975d73488c62b060489
Luiz Phelipe Soares Rodrigues <lphelipe@c1r7p3.42sp.org.br>
2022-09-11 16:52:02 -0300

    Adding tests for strchr

--- tests/tests_libft.c
+++ tests/tests_libft.c
@@ -138,6 +138,83 @@ MU_TEST_SUITE(test_strlcpy_entering_mochida_rapdos_0_do_not_modify_dst_and_retur
 	mu_assert_string_eq(expected_dst, dst);
 }
 
+MU_TEST_SUITE(test_strchr_look_up_non_existing_character_in_string_should_return_NULL)
+{
+	//ARRANGE
+	const char	string[] = "banana";
+	int			character = 'z';
+	char		*expected_result = NULL;
+	char		*actual_result;
+
+	//ACT
+	actual_result = ft_strchr(string, character);
+
+	//ASSERT
+	mu_check(expected_result == actual_result);
+}
+
+MU_TEST_SUITE(test_strchr_look_up_existing_character_in_string_should_return_its_address)
+{
+	//ARRANGE
+	char	string[] = "banana";
+	int			character = 'n';
+	char		*expected_result = &string[2]; //first 'n' address
+	char		*actual_result;
+
+	//ACT
+	actual_result = ft_strchr(string, character);
+
+	//ASSERT
+	mu_assert(expected_result == actual_result, "expected_result content is different from actual_result");
+}
+
+MU_TEST_SUITE(test_strchr_look_up_null_character_in_string_should_return_the_terminating_zero_address)
+{
+	//ARRANGE
+	char	string[] = "banana";
+	int			character = '\0';
+	char		*expected_result = &string[6]; // address of terminating '\0'
+	char		*actual_result;
+
+	//ACT
+	actual_result = ft_strchr(string, character);
+
+	//ASSERT
+	mu_assert(expected_result == actual_result, "expected_result should point to the terminating '\\0'");
+}
+
+MU_TEST_SUITE(test_strchr_look_up_character_in_empty_string_should_return_null)
+{
+	//ARRANGE
+	char	string[] = "";
+	int			character = 'c';
+	char		*expected_result = NULL;
+	char		*actual_result;
+
+	//ACT
+	actual_result = ft_strchr(string, character);
+
+	//ASSERT
+	mu_assert(expected_result == actual_result, "expected_result should be NULL");
+}
+
+MU_TEST_SUITE(test_strchr_look_up_character_in_null_string_should_return_null)
+{
+	//ARRANGE
+	char	string[] = "";
+	int			character = 'c';
+	char		*expected_result = NULL;
+	char		*actual_result;
+
+	//ACT
+	actual_result = ft_strchr(string, character);
+
+	//ASSERT
+	mu_assert(expected_result == actual_result, "expected_result should be NULL");
+}
+
+
+
 MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_strlcpy_entering_mochida_rapdos_0_do_not_modify_dst_and_returns_6);
 	MU_RUN_TEST(test_strlcpy_entering_lu_la_minus_1_set_dst_as_la_and_returns_2);
@@ -146,7 +223,12 @@ MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_strlcpy_entering_ah_boi_3_set_dst_as_bo_and_returns_3);
 	MU_RUN_TEST(test_strlcpy_entering_ah_oi_3_set_dst_as_oi_and_returns_2);
 	MU_RUN_TEST(test_isdigit_entering_c_returns_false);
+	MU_RUN_TEST(test_strchr_look_up_non_existing_character_in_string_should_return_NULL);
+	MU_RUN_TEST(test_strchr_look_up_existing_character_in_string_should_return_its_address);
+	MU_RUN_TEST(test_strchr_look_up_null_character_in_string_should_return_the_terminating_zero_address);
+	MU_RUN_TEST(test_strchr_look_up_character_in_empty_string_should_return_null);
 }
+
 int main() {
 	MU_RUN_SUITE(test_suite);
 	MU_REPORT();
